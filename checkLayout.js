4804478e4f1d0d7357e417d6faf3d8cd163004cd
Austin Turnage <austin@datarats.com>
2015-08-20 09:57:15 -0400

    check for jquery

--- checkLayout.js
+++ checkLayout.js
@@ -1,33 +1,35 @@
-$(document).ready(function() {
-	function checkLayout() {
-		function setLayoutLG() {
-			// Run contents for large screens
+if (!typeof jQuery == 'undefined' || window.jQuery) {
+	$(document).ready(function() {
+		function checkLayout() {
+			function setLayoutLG() {
+				// Run contents for large screens
+			}
+			function setLayoutMD() {
+				// Run contents for medium screens
+			}
+			function setLayoutSM() {
+				// Run contents for small screens
+			}
+			function setLayoutXS() {
+				//  Run contents for mobile screens
+			}
+			if ($('#check-lg').css("display") == "block") {
+				setLayoutLG();
+			}
+			if ($('#check-md').css("display") == "block") {
+				setLayoutMD();
+			}
+			if ($('#check-sm').css("display") == "block") {
+				setLayoutSM();
+			}
+			if ($('#check-xs').css("display") == "block") {
+				setLayoutXS();
+			}
 		}
-		function setLayoutMD() {
-			// Run contents for medium screens
-		}
-		function setLayoutSM() {
-			// Run contents for small screens
-		}
-		function setLayoutXS() {
-			//  Run contents for mobile screens
-		}
-		if ($('#check-lg').css("display") == "block") {
-			setLayoutLG();
-		}
-		if ($('#check-md').css("display") == "block") {
-			setLayoutMD();
-		}
-		if ($('#check-sm').css("display") == "block") {
-			setLayoutSM();
-		}
-		if ($('#check-xs').css("display") == "block") {
-			setLayoutXS();
-		}
-	}
-	var resizeTimer;
-	$(window).resize(function() {
-		clearTimeout(resizeTimer);
-		resizeTimer = setTimeout(checkLayout, 150);
+		var resizeTimer;
+		$(window).resize(function() {
+			clearTimeout(resizeTimer);
+			resizeTimer = setTimeout(checkLayout, 150);
+		});
 	});
-});
+}
